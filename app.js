9fee45f2bb6dfd649f9af7d76f0d3f25a4eea52d
Andy-Fu\Andy <f255431@gmail.com>
2018-01-17 23:09:07 +0800

    Encoding User Input

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+console.log(argv.a);
+var encodedAddress = encodeURIComponent(argv.a);
 
 request({
-    url: 'https://maps.google.com/maps/api/geocode/json?address=%E6%96%B0%E5%8C%97%E5%B8%82%E6%B1%90%E6%AD%A2%E5%8D%80%E5%A4%A7%E5%90%8C%E8%B7%AF%E4%B8%80%E6%AE%B5369%E8%99%9F',
+    url: `https://maps.google.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
     var latitude = body.results[0].geometry.location.lat;
