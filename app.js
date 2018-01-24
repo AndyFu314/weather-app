c14c5478963673bbabf00093f577291e469e4987
Andy@UDN_mbp <andy.fu@udngroup.com.tw>
2018-01-24 15:50:24 +0800

    Wiring Up Weather Search

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode.js');
+// const geocode = require('./geocode/geocode.js');
 
-const argv = yargs
-    .options({
-        a: {
-            demand: true,
-            alias: 'address',
-            describe: 'Address to fetch weather for',
-            string: true
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
+// const argv = yargs
+//     .options({
+//         a: {
+//             demand: true,
+//             alias: 'address',
+//             describe: 'Address to fetch weather for',
+//             string: true
+//         }
+//     })
+//     .help()
+//     .alias('help', 'h')
+//     .argv;
 
-geocode.geocodeAddress(argv.a, (errorMessage, results) => {
-    if (errorMessage){
-        console.log(errorMessage);
-    }else {
-        console.log(JSON.stringify(results, undefined, 2));
+// geocode.geocodeAddress(argv.a, (errorMessage, results) => {
+//     if (errorMessage){
+//         console.log(errorMessage);
+//     }else {
+//         console.log(JSON.stringify(results, undefined, 2));
+//     }
+// });
+
+// 3c52ed7a7a2ed4d16a86bb4aa3dfc793
+
+const request = require('request');
+
+const url = 'https://api.darksky.net/forecast/3c52ed7a7a2ed4d16a86bb4aa3dfc793/25.0612289,121.6407782';
+
+request({
+    uri: url,
+    json: true,
+}, (error, response, body) => {
+
+    if (!error && response.statusCode === 200){
+        var temperature = ((body.currently.temperature - 32) * 5 / 9).toFixed(2);
+        
+        console.log('temperature: ', temperature);
+    } else {
+        console.log('Unable to fetch weather.');
     }
 });
